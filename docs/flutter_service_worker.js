ad3e4a644b0dad959d7df9d0ad6bfd355fb0d0b4
wyklion <wyklion@qq.com>
2023-12-09 10:00:33 +0000

    update build web

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "8ddc7178275d6520cec4e8c638980e9a",
 "favicon.png": "ca8da297174aeee31b7885737db9fad8",
-"index.html": "3076460d014f5b6471e04dbeb1b080f6",
-"/": "3076460d014f5b6471e04dbeb1b080f6",
-"main.dart.js": "c01e83454fe5b402727b905e9c11712e",
+"index.html": "ceed246fad263ff781a391f0827c6bc8",
+"/": "ceed246fad263ff781a391f0827c6bc8",
+"main.dart.js": "c30f671018132d59fc8eb0a8d745bc80",
 "assets/FontManifest.json": "f28e381a20a4266c6868013164e190af",
 "assets/AssetManifest.json": "4d1d7504157617a6c7f6623f0e08817c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
