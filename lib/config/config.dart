7b119eed683db15fae99b9c372caafdea4fece31
KK <wyklion@qq.com>
2023-12-18 18:22:48 +0800

    广告ID配置不能乱改。

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,15 +2,15 @@ class Config {
   static const String version = 'v1.0.4';
   static const bool release = false;
   static bool testMode = false;
-  // static const String iosBannerId = 'ca-app-pub-1261971577301251/1507176294';
-  // static const String iosBonusId = 'ca-app-pub-1261971577301251/1392406479';
-  // static const String androidBannerId =
-  //     'ca-app-pub-1261971577301251/1804367801';
-  // static const String androidBonusId = 'ca-app-pub-1261971577301251/6537819735';
-  // test
-  static const String iosBannerId = 'ca-app-pub-3940256099942544/2934735716';
-  static const String iosBonusId = 'ca-app-pub-3940256099942544/1712485313';
+  static const String iosBannerId = 'ca-app-pub-1261971577301251/1507176294';
+  static const String iosBonusId = 'ca-app-pub-1261971577301251/1392406479';
   static const String androidBannerId =
-      'ca-app-pub-3940256099942544/6300978111';
-  static const String androidBonusId = 'ca-app-pub-3940256099942544/5224354917';
+      'ca-app-pub-1261971577301251/1804367801';
+  static const String androidBonusId = 'ca-app-pub-1261971577301251/6537819735';
+  // test
+  // static const String iosBannerId = 'ca-app-pub-3940256099942544/2934735716';
+  // static const String iosBonusId = 'ca-app-pub-3940256099942544/1712485313';
+  // static const String androidBannerId =
+  //     'ca-app-pub-3940256099942544/6300978111';
+  // static const String androidBonusId = 'ca-app-pub-3940256099942544/5224354917';
 }
